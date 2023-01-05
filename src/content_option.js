9487c6591973b522e42596baa7061fa78e63520a
Taylor Romoser <TRomoser@gmail.com>
2023-01-04 17:22:19 -0800

    changing info

--- src/content_option.js
+++ src/content_option.js
@@ -1,23 +1,23 @@
-const logotext = "JOHN";
+const logotext = "TAYLOR";
 const meta = {
-    title: "John Doe",
-    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+    title: "Taylor Romoser",
+    description: "I'm Taylor Romoser, full-stack engineer living in Seattle",
 };
 
 const introdata = {
-    title: "I’m John Doe",
+    title: "I’m Taylor Romoser",
     animated: {
         first: "I love coding",
         second: "I code cool websites",
-        third: "I develop mobile apps",
+        third: "I develop useful apps",
     },
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
-    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+    description: "I'm Taylor Romoser, a full-stack engineer living in Seattle.",
+    your_img_url: "https://i.imgur.com/m9F6x4g.jpg",
 };
 
 const dataabout = {
-    title: "abit about my self",
-    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+    title: "A bit about my self",
+    aboutme: "As a full-stack software engineer with a passion for the logic and elegance of code, I am excited to bring my unique perspective and customer service skills to the world of software development. With my background in customer service, I understand the importance of creating a dynamic and positive user experience. I am eager to continue learning and growing as a developer, and I am confident that my unique blend of skills and experiences make me a strong candidate for any software development role. Whether it's troubleshooting technical issues or developing engaging and user-friendly applications, I am ready to take on new challenges and make a meaningful contribution to my clients and peers.",
 };
 const worktimeline = [{
         jobtitle: "Designer of week",
@@ -136,9 +136,9 @@ const dataportfolio = [{
 ];
 
 const contactConfig = {
-    YOUR_EMAIL: "name@domain.com",
-    YOUR_FONE: "(555)123-4567",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    YOUR_EMAIL: "TRomoser@gmail.com",
+    YOUR_FONE: "(425) 466-1902",
+    description: "Get in touch with me. I'm looking for job opportunities, industry connections, and mentor possibilities.",
     // creat an emailjs.com account 
     // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
     YOUR_SERVICE_ID: "service_id",
@@ -147,9 +147,9 @@ const contactConfig = {
 };
 
 const socialprofils = {
-    github: "https://github.com",
+    github: "https://github.com/TRomoser",
     facebook: "https://facebook.com",
-    linkedin: "https://linkedin.com",
+    linkedin: "https://www.linkedin.com/in/taylor-romoser/",
     twitter: "https://twitter.com",
     youtube: "https://youtube.com",
     twitch: "https://twitch.com",
